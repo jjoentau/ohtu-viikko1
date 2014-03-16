f77372bec1f5f6fb812fe0bdcd476d60fec7539f
Johanna <jjoentau@cs.helsinki.fi>
2014-03-16 13:14:13 +0200

    fiksatt

--- OhtuVarasto/src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ OhtuVarasto/src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -88,7 +88,7 @@ public class VarastoTest {
         varasto.lisaaVarastoon(8);
 
         varasto.otaVarastosta(2);
-
+ 
         // varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         assertEquals(4, varasto.paljonkoMahtuu(), vertailuTarkkuus);
     }
